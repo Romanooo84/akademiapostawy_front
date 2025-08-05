bc3b2324d2153610793d2739b6a459b3648bc8f3
Pisarski Roman <pisarski.roman@gmail.com>
2025-08-05 11:58:10 +0200

    update ready to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 
-import './App.css'
+import { Route, Routes} from "react-router-dom";
+import SharedLayout from "./components/sharedLayout/SharedLayout";
 
 
 function App() {
 
   return (
-    <>
-      <div>
-       <p>Akademia postawy</p>
-      </div>
-    </>
+     <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        {/*<Route path="/picture of a day" element={<PictureOfADay/>}/>
+        <Route path="/gallery" element={<Gallery/>}/>
+        <Route path="/Mars Pictures" element={<PictureOfMars/>}/>
+        <Route path="/Near Earth Object" element={<NearObjectDetails/>}/>*/}
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/components/sharedLayout/SharedLayout.jsx
@@ -0,0 +1,24 @@
+import Header  from "../header/header"
+import Footer from "../footer/footer"
+import Home from '../home/home'
+import { Outlet, useLocation  } from "react-router-dom";
+
+const SharedLayout = () => {
+    const location = useLocation();
+    const { pathname } = location;
+
+ return (
+  <>
+    <Header />
+    <main>
+      <div>
+        {pathname === '/' ? <Home /> : <Outlet />}
+      </div>
+    </main>
+    <Footer />
+  </>
+);
+
+}
+
+export default SharedLayout
