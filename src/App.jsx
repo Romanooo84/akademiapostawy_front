6471599035582adc2684f437ee24845807805913
Pisarski Roman <pisarski.roman@gmail.com>
2025-08-06 10:15:57 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 
 import { Route, Routes} from "react-router-dom";
 import SharedLayout from "./components/sharedLayout/SharedLayout";
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+   useEffect(() => {
+    fetch('https://srv80578.seohost.com.pl/myvideolist')
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Server error: ${res.status}`);
+        }
+        console.log(res);
+        return res.json(); // lub .text(), .blob() zależnie od odpowiedzi
+      })
+      .then(json => setData(json))
+      .catch(err => setError(err.message));
+  }, [])
+
 
   return (
      <Routes>
