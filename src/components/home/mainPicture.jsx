64e9c89b44c7f8f51b10093f59f2ac40a5e4dae5
Pisarski Roman <pisarski.roman@gmail.com>
2025-08-07 15:52:36 +0200

    mainImage full

--- src/components/home/mainPicture.jsx
+++ src/components/home/mainPicture.jsx
@@ -1,40 +1,44 @@
 import { useEffect, useState } from 'react';
-import css from './mainPicture.module.css'; // Assuming you have a CSS file for styling
+import css from './mainPicture.module.css';
 
 import picture1 from '../../images/przedszkole.jpg';
-import picture2 from '../../images/zajecia.jpg'
+import picture2 from '../../images/zajecia.jpg';
 import picture3 from '../../images/skolioza1.jpg';
 import picture4 from '../../images/skolioza2.jpg';
 import picture5 from '../../images/skolioza3.jpg';
 import picture6 from '../../images/wadypostawy.jpg';
 
 const MainPicture = () => {
+  const pictureList = [picture1, picture2, picture3, picture4, picture5, picture6];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [loaded, setLoaded] = useState(false);
 
-    const pictureList = [picture1, picture2, picture3, picture4, picture5, picture6];
-    const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    // Opóźnione przypisanie klasy active do pierwszego zdjęcia
+    const timeout = setTimeout(() => setLoaded(true), 50);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex(prevIndex => (prevIndex + 1) % pictureList.length);
-        }, 10000); // 10000 ms = 10 sekund
+    const interval = setInterval(() => {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % pictureList.length);
+    }, 8000); // co 10 sek
 
-        // Wyczyść interwał przy odmontowaniu komponentu
-        return () => clearInterval(interval);
-    }, [pictureList.length]);
+    return () => {
+      clearTimeout(timeout);
+      clearInterval(interval);
+    };
+  }, []);
 
-
-    return (
-        <div className={css.mainDiv}>
-            {pictureList.map((pic, index) => (
-                <img
-                    key={index}
-                    src={pic}
-                    alt={`Slajd ${index + 1}`}
-                    className={`${css.imageSlide} ${index === currentIndex ? css.active : ''}`}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className={css.mainDiv}>
+      {pictureList.map((pic, index) => (
+        <img
+          key={index}
+          src={pic}
+          alt={`Slajd ${index + 1}`}
+          className={`${css.imageSlide} ${index === currentIndex && loaded ? css.active : ''}`}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MainPicture;
