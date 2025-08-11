3fb1cdefeb1b6b3c3dd25e3a8166b876e24e82fd
Pisarski Roman <pisarski.roman@gmail.com>
2025-08-11 13:06:55 +0200

    text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./components/sharedLayout/SharedLayout";
 import Video from "./pages//video/video";
+import Blog from "./pages/blog/blog.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
-        <Route path="Blog" element={<Video />} />
+        <Route path="Blog" element={<Blog/>} />
         <Route path="projekty" element={<Video />} />
         <Route path="fizjomemo" element={<Video />} />
         <Route path="filmy" element={<Video />} />
